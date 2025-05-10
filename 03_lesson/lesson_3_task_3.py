80fb4248ba57bad4be51968d68d55374a3630167
Victoriya <@.com>
2025-05-10 17:16:56 +0200

    Чтобы запустить файлы, необходимо нажать на кнопку проигрывания

--- 03_lesson/lesson_3_task_3.py
+++ 03_lesson/lesson_3_task_3.py
@@ -1,7 +1,7 @@
-from adress import Address
+from address import Address
 from mailing import Mailing
 
-adress = Address("236038", "Калининград", "Юрия Гагарина", "3", "45")
+address = Address("236038", "Калининград", "Юрия Гагарина", "3", "45")
 from_address1 = Address("234856", "Москва", "Дзержинского", "23", "56")
 from_address2 = Address("213456", "Зеленоград", "Погодина", "46", "789")
 from_address3 = Address("345678", "Волгоград", "Непройдёнова", "123", "345")
@@ -14,6 +14,6 @@ track1 = "234Tr56"
 track2 = "678yu5645y"
 track3 = "ghj456vv43"
 
-mailing = Mailing(adress,  [from_address1, from_address2, from_address3], [cost1, cost2, cost3], [track1, track2, track3])
+mailing = Mailing(address,  [from_address1, cost1, track1], [from_address2, cost2, track2], [from_address3, cost2, track3])
 
 print(mailing)
